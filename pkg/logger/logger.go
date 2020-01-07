05f29cb45dc27bed4239da7191b65403b19150c0
mak <mak@nodefactory.io>
2020-01-07 12:58:20 +0100

    Add logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+)
+
+func init()  {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetLevel(log.WarnLevel)
+	log.SetFormatter(&log.TextFormatter{
+		DisableTimestamp: false,
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			s := strings.Split(f.Function, ".")
+			funcname := s[len(s)-1]
+			_, filename := path.Split(f.File)
+			return funcname, filename
+		},
+	})
+	// TODO make logger write to file and change format of logging
+	log.SetOutput(os.Stdout)
+}

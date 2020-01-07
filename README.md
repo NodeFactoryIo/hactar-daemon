2965de3073629346ba9f3826c2ca9668f9171bd8
mak <mak@nodefactory.io>
2020-01-07 12:57:56 +0100

    Clean main, add viper configuration, update README and remove cmd files

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # hactar-daemon
 Hactar daemon app built using Go for communication with Filecoin lotus-node.
+
+```
+hactar-deamon is a command line interface for ....
+
+Usage:
+  hactar [command]
+
+Available Commands:
+  start       Starts hactar deamon app
+  token       Display lotus token
+  help        Help about any command
+  version     Show the current version of Hactar deamon app
+
+Start:
+
+    Starts hactar deamon app, connects with hactar service and starts monitoring process.
+
+    Flags:
+      -u, --username string         Username
+      -p, --password string         Password
+      -h, --help                    help for start command
+
+Use "hactar [command] --help" for more information about a command.
+```
+
+####Example
+Starting hactar without using flags.
+```
+    hactar start
+```
+```
+    Enter lotus username: XXX
+    Enter lotus password: XXX
+    Starting hactar.....
+```
--- main.go
+++ main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"fmt"
-	"github.com/NodeFactoryIo/hactar-daemon/cmd"
+	"github.com/NodeFactoryIo/hactar-daemon/cmd/hactar/commands"
+	"github.com/NodeFactoryIo/hactar-daemon/internal/util"
+	_ "github.com/NodeFactoryIo/hactar-daemon/pkg/logger" // initialize logger
+	"github.com/mkideal/cli"
+	"github.com/spf13/viper"
 	"os"
+	"strconv"
 )
 
+// Load configuration and initialize commands
 func main() {
-	must(cmd.RootCmd.Execute())
-}
+	// load config file
+	viper.SetConfigName("config") 			// name of config file (without extension)
+	viper.AddConfigPath("/etc/appname/")   	// path to look for the config file in
+	viper.AddConfigPath("$HOME/.appname")  	// call multiple times to add many search paths
+	viper.AddConfigPath(".")               	// optionally look for config in the working directory
+	util.Must(viper.ReadInConfig(), "Fatal error reading config file")
 
-func must(err error) {
-	if err != nil {
-		fmt.Println(err.Error())
+	// start program
+	fmt.Println("Starting hactar....")
+	interval, _ := strconv.Atoi(viper.GetString("stats.interval"))
+	fmt.Printf("Stats refresh interval is %d minutes.\n", interval / 60)
+
+	// initialize commands
+	if err := cli.Root(commands.RootCommand,
+		cli.Tree(cli.HelpCommand("Display help information")),
+		cli.Tree(commands.StartCommand),
+		cli.Tree(commands.TokenCommand),
+	).Run(os.Args[1:]); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
+
+

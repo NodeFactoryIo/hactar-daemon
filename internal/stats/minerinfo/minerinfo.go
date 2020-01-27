82a918f4b287efb19dc114f44ef4210104114df1
mak <mak@nodefactory.io>
2020-01-27 22:38:33 +0100

    Handle error case

--- internal/stats/minerinfo/minerinfo.go
+++ internal/stats/minerinfo/minerinfo.go
@@ -10,25 +10,25 @@ import (
 func SendMinerInfoStats(hactarClient *hactar.Client, lotusClient *lotus.Client) bool {
 	minerAddress, err := lotusClient.Miner.GetMinerAddress()
 	if err != nil {
-		// TODO
+		log.Error("Unable to get miner address ", err)
 		return false
 	}
 
 	minerStats, err := lotusClient.Miner.GetMinerPower(minerAddress)
 	if err != nil {
-		// TODO
+		log.Error("Unable to get miner power ", err)
 		return false
 	}
 
 	sectorSize, err := lotusClient.Sectors.GetSectorSize(minerAddress)
 	if err != nil {
-		// TODO
+		log.Error("Unable to get sector size ", err)
 		return false
 	}
 
 	clientVersion, err := lotusClient.Client.GetClientVersion()
 	if err != nil {
-		// TODO
+		log.Error("Unable to get client version ", err)
 		return false
 	}
 
@@ -42,10 +42,10 @@ func SendMinerInfoStats(hactarClient *hactar.Client, lotusClient *lotus.Client) 
 	response, err := hactarClient.Miner.SendMinerInfo(*minerInfo)
 
 	if response != nil && response.StatusCode == http.StatusOK {
-		log.Info()
+		log.Info("Miner info successfully sent to hactar")
 		return true
 	}
 
-	log.Error("Unable to send miner information statistics. ", err)
+	log.Error("Unable to send miner information statistics ", err)
 	return false
 }

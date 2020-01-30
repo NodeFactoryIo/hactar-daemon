18e81149c2a1a7f0bc834200e9f4583a25b0fffc
mak <mak@nodefactory.io>
2020-01-30 13:15:49 +0100

    Fix block test

--- internal/hactar/blocks_test.go
+++ internal/hactar/blocks_test.go
@@ -12,24 +12,30 @@ func TestBlocksService_AddMiningReward(t *testing.T) {
 	setup()
 	defer teardown()
 
-	addRequest := &Block{
-		Cid:   "test-cid",
-		Miner: "t0101",
+	addRequest := []Block{
+		*(&Block{
+			Cid:   "test-cid-1",
+			Miner: "t0101",
+		}),
+		*(&Block{
+			Cid:   "test-cid-2",
+			Miner: "t0101",
+		}),
 	}
 
 	mux.HandleFunc(AddBlockRewardPath, func(w http.ResponseWriter, r *http.Request) {
-		v := new(Block)
+		v := new([]Block)
 		err := json.NewDecoder(r.Body).Decode(v)
 		// assert valid request
 		assert.Nil(t, err)
 		assert.Equal(t, http.MethodPost, r.Method)
-		assert.Equal(t, addRequest, v)
+		assert.Equal(t, addRequest, *v)
 
 		resp, _ := json.Marshal(addRequest)
 		_, _ = fmt.Fprintf(w, fmt.Sprintf(`%s`, string(resp)))
 	})
 
-	response, err := client.Blocks.AddMiningReward(*addRequest)
+	response, err := client.Blocks.AddMiningReward(addRequest)
 	// assert valid response
 	assert.NotNil(t, response)
 	assert.Nil(t, err)
